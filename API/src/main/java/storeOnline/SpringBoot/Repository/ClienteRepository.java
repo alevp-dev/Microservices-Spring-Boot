23d8937b133e7c3b7f512db46324359ba3022138
Alejandra Villa Posada <133827712+alevp-dev@users.noreply.github.com>
2025-05-16 15:11:11 -0500

    Delete API directory

--- API/src/main/java/storeOnline/SpringBoot/Repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package storeOnline.SpringBoot.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import storeOnline.SpringBoot.Entity.ClienteEntity;
-
-@Repository
-public interface ClienteRepository extends JpaRepository<ClienteEntity, Long> {
-}
--- API/src/main/java/storeOnline/SpringBoot/Repository/OrdenDetalleRepository.java
+++
@@ -1,9 +0,0 @@
-package storeOnline.SpringBoot.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import storeOnline.SpringBoot.Entity.OrdenDetalleEntity;
-
-@Repository
-public interface OrdenDetalleRepository extends JpaRepository<OrdenDetalleEntity, Long> {
-}
