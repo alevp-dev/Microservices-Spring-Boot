16217539d6cd0d6328beaaec96c93e5c4d78eeb8
Alejandra Villa Posada <133827712+alevp-dev@users.noreply.github.com>
2025-05-16 14:01:21 -0500

    Delete SpringBoot directory

--- SpringBoot/src/main/java/storeOnline/SpringBoot/Repository/InventarioRepository.java
+++
@@ -1,9 +0,0 @@
-package storeOnline.SpringBoot.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import storeOnline.SpringBoot.Entity.InventarioEntity;
-
-@Repository
-public interface InventarioRepository extends JpaRepository<InventarioEntity, Long> {
-}
--- SpringBoot/src/main/java/storeOnline/SpringBoot/Repository/NotificadorRepository.java
+++
@@ -1,9 +0,0 @@
-package storeOnline.SpringBoot.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import storeOnline.SpringBoot.Entity.NotificadorEntity;
-
-@Repository
-public interface NotificadorRepository extends JpaRepository<NotificadorEntity, Long> {
-}
